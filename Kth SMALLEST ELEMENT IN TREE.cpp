82c6500f437ec4e8a251356ea8b4730919b784ab
Vibhuti0206 <56320137+Vibhuti0206@users.noreply.github.com>
2020-03-31 19:16:48 +0530

    Create Kth SMALLEST ELEMENT IN TREE.cpp

---
+++ Kth SMALLEST ELEMENT IN TREE.cpp
@@ -0,0 +1,13 @@
+int find(TreeNode* root, int &k) {
+            if (root == NULL)  return -1;
+            // We do an inorder traversal here. 
+            int k1 = find(root->left, k);
+            if (k == 0) return k1; // left subtree has k or more elements.
+            k--; 
+            if (k == 0) return root->val; // root is the kth element.
+            return find(root->right, k); // answer lies in the right node.
+        }
+
+int Solution::kthsmallest(TreeNode* root, int k) {
+    return find(root, k);
+}
